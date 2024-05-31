fecd7415dc9227ae255a2530ff920212fe453c89
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-05-31 17:11:12 +0530

    Second update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export const Header = () => {
       <div>
         <h1 className="bannerText">React JS Tutorial with Aplications</h1>
         <p className="slogan">Learn more be smart and achieve everything</p>
-        <p></p>
+        <p>Once we familiar with ReactJS we can make lot's pf applications</p>
       </div>
     </>
   );
