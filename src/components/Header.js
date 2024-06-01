e4368fd2ae01ecf13163392f165ac28689894c4e
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-06-01 20:45:24 +0530

    Conditional Rendering

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 export const Header = () => {
-  let customCSS ="code"
-  let customCSS2="error"
+  let customCSS ="code";
+  let customCSS2="error";
+  const isLoggedin= false;
+  const greeting = isLoggedin ? <p>Welcome to Uki</p> : <p>Please Logged in first</p>;
   return (
     <>
       <div>
@@ -8,7 +10,9 @@ export const Header = () => {
         <p className="slogan">Learn more be smart and achieve everything</p>
         <p>Once we familiar with ReactJS we can make lot's pf applications</p>
         <p className={customCSS}>20+30={20+30}</p>
-        <p className={customCSS2}>This message shows error</p>
+        <p className={customCSS2} style={{fontSize:'50px'}}>This message shows error</p>
+        {/* {Conditional Rendering} */}
+        {greeting}
       </div>
     </>
   );
