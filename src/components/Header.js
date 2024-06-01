1168ff4c27bc9839fd8f4ba2ae8a169f39a9a620
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-06-01 20:22:00 +0530

    Javascript expression update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 export const Header = () => {
+  let customCSS ="code"
+  let customCSS2="error"
   return (
     <>
       <div>
         <h1 className="bannerText">React JS Tutorial with Aplications</h1>
         <p className="slogan">Learn more be smart and achieve everything</p>
         <p>Once we familiar with ReactJS we can make lot's pf applications</p>
+        <p className={customCSS}>20+30={20+30}</p>
+        <p className={customCSS2}>This message shows error</p>
       </div>
     </>
   );
