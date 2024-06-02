3a126d11fa7a63e2088e33581620eba07a2f3658
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-06-02 12:55:58 +0530

    Props children added  on June 2nd

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export const Header = () => {
       <div>
         <h1 className="bannerText">React JS Tutorial with Aplications</h1>
         <p className="slogan">Learn more be smart and achieve everything</p>
-        <p>Once we familiar with ReactJS we can make lot's pf applications</p>
+        <p>Once we familiar with ReactJS we can make lot's of applications</p>
         
         {/* {JavaScript expression} */}
         <p className={customCSS}>20+30={20+30}</p>
